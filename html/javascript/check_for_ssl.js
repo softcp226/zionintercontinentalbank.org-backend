60b1440ae66486c96c239cfc0f34774475544094
softcp226 <softcp226@gmail.com>
2022-07-18 23:49:00 -0700

    disable ssl check

--- html/javascript/check_for_ssl.js
+++ html/javascript/check_for_ssl.js
@@ -1,8 +1,8 @@
-(() => {
-  if (
-    window.location.href.includes("http://www.zionintercontinentalb.com") ||
-    window.location.href.includes("http://zionintercontinentalb.com")
-  ) {
-    window.location.replace("https://www.zionintercontinentalb.com/");
-  }
-})();
+// (() => {
+//   if (
+//     window.location.href.includes("http://www.zionintercontinentalb.com") ||
+//     window.location.href.includes("http://zionintercontinentalb.com")
+//   ) {
+//     window.location.replace("https://www.zionintercontinentalb.com/");
+//   }
+// })();
